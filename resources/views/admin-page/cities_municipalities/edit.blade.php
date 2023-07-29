61b3eff4d5f431503780c1e787dcae307a24e2d5
James Garnfil <jamesgarnfil15@gmail.com>
2023-07-29 11:34:28 +0800

    update edit city municipality form

--- resources/views/admin-page/cities_municipalities/edit.blade.php
+++ resources/views/admin-page/cities_municipalities/edit.blade.php
@@ -27,7 +27,8 @@
                                 </ul>
                             </div>
                         @endif
-                        <form action="{{ route('admin.city_municipality.update', $data->id) }}" method="post" enctype="multipart/form-data">
+                        <form action="{{ route('admin.city_municipality.update', $data->id) }}" method="post"
+                            enctype="multipart/form-data">
                             @method('PUT')
                             @csrf
                             <input type="hidden" name="old_image" value="{{ $data->featured_image }}">
@@ -36,23 +37,32 @@
                                     <div class="row">
                                         <div class="col-lg-6 my-1">
                                             <label for="name" class="form-label">Name</label>
-                                            <input type="text" class="form-control" name="name" id="name" value="{{ $data->name }}">
+                                            <input type="text" class="form-control" name="name" id="name"
+                                                value="{{ $data->name }}">
                                         </div>
                                         <div class="col-lg-6 my-1">
-                                            <label for="featured_image" class="form-label">Featured Image  <span class="text-primary primary" style="font-size: 10px;">Change it if needed.</span></label>
-                                            <input type="file" class="form-control" name="featured_image" id="featured_image" value="">
+                                            <label for="featured_image" class="form-label">Featured Image <span
+                                                    class="text-primary primary" style="font-size: 10px;">Change it if
+                                                    needed.</span></label>
+                                            <input type="file" class="form-control" name="featured_image"
+                                                id="featured_image" value="">
                                         </div>
                                         <div class="col-lg-6 my-1">
                                             <label for="featured_image" class="form-label">Type</label>
                                             <fieldset>
                                                 <div class="custom-control custom-radio">
-                                                    <input type="radio" class="custom-control-input" name="type" id="type1" value="city" {{ $data->type == 'city' ? 'checked' : null }}>
+                                                    <input type="radio" class="custom-control-input" name="type"
+                                                        id="type1" value="city"
+                                                        {{ $data->type == 'city' ? 'checked' : null }}>
                                                     <label class="custom-control-label" for="type1">City</label>
                                                 </div>
                                             </fieldset>
                                             <fieldset>
                                                 <div class="custom-control custom-radio">
-                                                    <input type="radio" class="custom-control-input" name="type" id="type2" {{ $data->type == 'municipality' ? 'checked' : null }} value="municipality">
+                                                    <input type="radio" class="custom-control-input" name="type"
+                                                        id="type2"
+                                                        {{ $data->type == 'municipality' ? 'checked' : null }}
+                                                        value="municipality">
                                                     <label class="custom-control-label" for="type2">Municipality</label>
                                                 </div>
                                             </fieldset>
@@ -71,16 +81,18 @@
                                             <select name="province_id" id="province" class="select2 form-control">
                                                 <option value="">--- SELECT PROVINCE ---</option>
                                                 @foreach ($provinces as $province)
-                                                    <option {{ $data->province_id == $province->id ? 'selected' : null }} value="{{ $province->id }}">{{ $province->name }}</option>
+                                                    <option {{ $data->province_id == $province->id ? 'selected' : null }}
+                                                        value="{{ $province->id }}">{{ $province->name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-lg-4">
-                                    @if($province->featured_image)
+                                    @if ($province->featured_image)
                                         <img class="img-responsive" id="previewImage" style="width: 100% !important;"
-                                        src="{{ URL::asset('app-assets/images/provinces/' . $province->featured_image) }}" alt="">
+                                            src="{{ URL::asset('app-assets/images/city_municipality/' . $province->featured_image) }}"
+                                            alt="">
                                     @endif
                                 </div>
                             </div>
@@ -99,22 +111,21 @@
     <script>
         // Function to handle file selection and display preview image
         function handleFileSelect(event) {
-        const file = event.target.files[0];
+            const file = event.target.files[0];
 
-        if (file) {
-            const reader = new FileReader();
+            if (file) {
+                const reader = new FileReader();
 
-            reader.onload = function(event) {
-            const previewImage = document.getElementById('previewImage');
-            previewImage.src = event.target.result;
-            };
+                reader.onload = function(event) {
+                    const previewImage = document.getElementById('previewImage');
+                    previewImage.src = event.target.result;
+                };
 
-            reader.readAsDataURL(file);
-        }
+                reader.readAsDataURL(file);
+            }
         }
 
         // Attach the 'handleFileSelect' function to the file input's change event
         document.getElementById('featured_image').addEventListener('change', handleFileSelect);
-
     </script>
 @endpush
